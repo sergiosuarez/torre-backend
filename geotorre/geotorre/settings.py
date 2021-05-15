d1ac2571c7eccc05dc74598e0afa4b4bbd52a909
root <root@vmi543651.contaboserver.net>
2021-05-15 18:54:52 -0500

    add cors headers to deploy in the server

--- geotorre/geotorre/settings.py
+++ geotorre/geotorre/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ozb-ks4j+n$$t_aiqo6g1o$((@up#7$-ox&s2*q+*giqzi=b^_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['209.145.62.164']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'geotorre.urls'
@@ -131,3 +133,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
+
